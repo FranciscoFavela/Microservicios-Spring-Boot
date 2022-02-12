b0e9fb3ebc4cac0ae223cc168befcc90980585e1
FranciscoFavelaNajera <98063834+FranciscoFavelaNajera@users.noreply.github.com>
2022-02-11 18:08:31 -0600

    Delete client-service directory

--- client-service/src/main/java/com/microservicios/clientservice/repositories/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.microservicios.clientservice.repositories;
-
-import com.microservicios.clientservice.entities.Client;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface ClientRepository extends BaseRepository<Client,Long> {
-
-}
